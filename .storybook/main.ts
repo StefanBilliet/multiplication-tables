55e1f8250f8de4615fcfeb06719c52bb011c52a7
Meester Stefan <stefan.billiet@corilus.be>
2026-03-17 10:13:27 +0100

    chore: silence Storybook preview chunk warning

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,6 +4,13 @@ const config: StorybookConfig = {
   framework: "@storybook/react-vite",
   stories: ["../src/**/*.stories.@(ts|tsx)"],
   addons: [],
+  viteFinal: async (config) => ({
+    ...config,
+    build: {
+      ...config.build,
+      chunkSizeWarningLimit: 1300,
+    },
+  }),
 };
 
 export default config;
